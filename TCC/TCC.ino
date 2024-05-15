83996d492ea83f5e0a3f39ef8efaa91bb02cd995
Kedesdt <kedesdt@gmail.com>
2024-05-15 11:28:24 -0300

    adicionados alguns comentarios e Macros

--- TCC/TCC.ino
+++ TCC/TCC.ino
@@ -8,6 +8,7 @@
 
 #define REPORTING_PERIOD_MS     10000
 #define LED_PIN 2
+#define PULSEIRA_ID 2
 
 //const char* ssid = "KJAA";
 //const char* password =  "kjaa2017";
@@ -15,17 +16,20 @@ const char* ssid = "renato";
 const char* password =  "lablab01";
 const char* serverName = "http://45.93.100.30:8080/data";
 
+
+// variaveis
 int lastValidOxi, lastValidFreq, oxi, freq, lastValidTemp, temp, axi, ayi, azi, gxi, gyi, gzi;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
-PulseOximeter pox;
-Generic_LM75 temperature;
-MPU6050 accelgyro;
+PulseOximeter pox; // objeto que obtem pulsação e oxigenaçao
+Generic_LM75 temperature; // objeto que obtem temperatura
+MPU6050 accelgyro; //objeto que obtem aceleração e giroscopio
 
 uint32_t tsLastReport = 0;
 
-// Callback (registered below) fired when a pulse is detected
+
+// Função callback que roda sempre que um batimento é detectado
 void onBeatDetected()
 {
   Serial.println("Beat");
@@ -38,21 +42,20 @@ void onBeatDetected()
 void setup()
 {
   Serial.begin(115200);
-
+  
   Serial.print("Initializing pulse oximeter..");
-
-    // Initialize the PulseOximeter instance
-    // Failures are generally due to an improper I2C wiring, missing power supply
-    // or wrong target chip
   if (!pox.begin()) {
     Serial.println("FAILED");
     for(;;);
   } else {
     Serial.println("SUCCESS");
   }
+
   pinMode(LED_PIN, OUTPUT);
+
   pox.setOnBeatDetectedCallback(onBeatDetected);
   pox.setIRLedCurrent(MAX30100_LED_CURR_50MA);
+
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     Wire.begin();
   #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
@@ -60,6 +63,7 @@ void setup()
   #endif
 
   accelgyro.initialize();
+
   Serial.println("Testing device connections...");
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
@@ -68,89 +72,91 @@ void setup()
 void loop()
 {
   pox.update();
-  //if ((WiFi.status() == WL_CONNECTED)){
-      // Make sure to call update as fast as possible
-    // Asynchronously dump heart rate and oxidation levels to the serial
-    // For both, a value of 0 means "invalid"
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
 
-      freq = pox.getHeartRate();
-      Serial.print("Heart rate:");
-      Serial.print(freq);
-      lastValidFreq = freq > 0 ? freq : lastValidFreq;
-      oxi = pox.getSpO2();
-      Serial.print("bpm / SpO2:");
-      Serial.print(oxi);
-      lastValidOxi = oxi > 0 ? oxi : lastValidOxi;
-      Serial.println("%");
-      temp = temperature.readTemperatureC();
-      lastValidTemp = temp ? temp : lastValidTemp;
-      Serial.print("Temperature = ");
-      Serial.print(lastValidTemp);
-      Serial.println(" C");
+    freq = pox.getHeartRate(); // obtendo pulsação
+    Serial.print("Heart rate:");
+    Serial.print(freq);
+    lastValidFreq = freq > 0 ? freq : lastValidFreq; // verificando se é uma pulsação valida senão mantem a ultima
+    
+    oxi = pox.getSpO2(); // obtendo oxigenaçao
+    Serial.print("bpm / SpO2:");
+    Serial.print(oxi);
+    lastValidOxi = oxi > 0 ? oxi : lastValidOxi; // verificando se é uma oxigenaçao valida senão mantem a ultima
+    Serial.println("%");
+    
+    temp = temperature.readTemperatureC(); // obtendo temperatura
+    lastValidTemp = temp ? temp : lastValidTemp; // verificando se é uma temperatura valida senão mantem a ultima
+    Serial.print("Temperature = ");
+    Serial.print(lastValidTemp);
+    Serial.println(" C");
 
-      accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-      axi = ax;
-      ayi = ay;
-      azi = az;
-      gxi = gx;
-      gyi = gy;
-      gzi = gz;
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); // obtendo aceleração e giroscopio
+    axi = ax;
+    ayi = ay;
+    azi = az;
+    gxi = gx;
+    gyi = gy;
+    gzi = gz;
 
-      Serial.print("Ax: " + String(ax)  + " ");
-      Serial.print("Ay: " + String(ay)  + " ");
-      Serial.println("Az: " + String(az)  + " ");
-      WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
-      }
-      Serial.println("Connected");
-      
-      HTTPClient http;
-      http.begin(serverName);
-      http.addHeader("Content-Type", "application/json");
+    Serial.print  ("Ax: " + String(ax)  + " ");
+    Serial.print  ("Ay: " + String(ay)  + " ");
+    Serial.println("Az: " + String(az)  + " ");
+    Serial.print  ("Gx: " + String(gx)  + " ");
+    Serial.print  ("Gy: " + String(gy)  + " ");
+    Serial.println("Gz: " + String(gz)  + " ");
 
-      String send = String() + 
-      "{"
-        "\"ax\": " + String(axi) + ", "
-        "\"ay\": " + String(ayi) + ", "
-        "\"az\": " + String(azi) + ", "
-        "\"gx\": " + String(gxi) + ", "
-        "\"gy\": " + String(gyi) + ", "
-        "\"gz\": " + String(gzi) + ", "
-        "\"freq\": " + String(lastValidFreq) + ", " 
-        "\"oxi\": "  + String(lastValidOxi)  + ", " 
-        "\"temp\" :" + String(lastValidTemp) + ", "
-        "\"pulseiraId\": 2"
-      "}";
-
-      Serial.println(send);
-
-      int httpResponseCode = http.POST(send);
-
-      if (httpResponseCode>0) {
-        String response = http.getString(); //Get the response to the request
-        Serial.println(httpResponseCode); //Print return code
-        Serial.println(response); //Print request answer
-      } else {
-        Serial.print("Error on sending POST: ");
-        Serial.println(httpResponseCode);
-      }
-
-      http.end(); //Free resources
-      
-      tsLastReport = millis();
-      WiFi.disconnect();
-      WiFi.mode(WIFI_OFF);
-      Serial.println("Wifi Disconnected");
-      //delay(1000);
-      if (!pox.begin()) {
-        Serial.println("FAILED");
-        for(;;);
-      } else {
-        Serial.println("SUCCESS");
-      }
+    WiFi.begin(ssid, password);  // conectando ao wifi somente agora pois não sei o porque mas conectado ao wifi o medidos de oxigenação e pulsação não funciona.
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
     }
-  //}
+    Serial.println("Connected");
+    
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+
+    // formando a string no formato json para envio do POST
+    String send = String() + 
+    "{"
+      "\"ax\": " + String(axi) + ", "
+      "\"ay\": " + String(ayi) + ", "
+      "\"az\": " + String(azi) + ", "
+      "\"gx\": " + String(gxi) + ", "
+      "\"gy\": " + String(gyi) + ", "
+      "\"gz\": " + String(gzi) + ", "
+      "\"freq\": " + String(lastValidFreq) + ", " 
+      "\"oxi\": "  + String(lastValidOxi)  + ", " 
+      "\"temp\" :" + String(lastValidTemp) + ", "
+      "\"pulseiraId\":" + String(PULSEIRA_ID) +
+    "}";
+
+    Serial.println(send);
+
+    int httpResponseCode = http.POST(send); // enviando post e pegando o codigo da reposta
+
+    if (httpResponseCode>0) {
+      String response = http.getString(); // pegando resposta
+      Serial.println(httpResponseCode); //Print return code
+      Serial.println(response); //Print request answer
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end(); //Free resources
+    
+    tsLastReport = millis();
+    WiFi.disconnect();
+    WiFi.mode(WIFI_OFF); // deconectar e desligar o wifi pois não funciona wifi r sensor de oxigenação e pulsação ao mesmo tempo
+    Serial.println("Wifi Disconnected");
+
+    if (!pox.begin()) {    // reiniciar o sensor de pulsação e oxigenação por causa do problema com o wifi
+      Serial.println("FAILED");
+      for(;;);
+    } else {
+      Serial.println("SUCCESS");
+    }
+  }
 }
